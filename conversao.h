d598483e444c5b4b3ef03d67b0b7620fb4f2306a
gabriel <gabrielfurniel.gc@outlook.com>
2023-06-18 23:38:05 -0300

    [DEL] função para gerar binarios em conversao.h

--- conversao.h
+++ conversao.h
@@ -42,32 +42,3 @@ void printBinaryUtil(int binary[], int n, int i, int *count, int **binaries)
   printBinaryUtil(binary, n, i + 1, count, binaries);
 }
 
-int **generateBinaries(int n, int *count)
-{
-  // Calcula o número total de binários possíveis
-  int totalBinaries = 1 << n;
-
-  // Aloca memória para armazenar os binários
-  int **binaries = (int **)malloc(totalBinaries * sizeof(int *));
-  for (int i = 0; i < totalBinaries; i++)
-  {
-    binaries[i] = (int *)malloc(n * sizeof(int));
-  }
-
-  // Chama a função auxiliar para gerar os binários
-  int binary[n];
-  //printBinaryUtil(binary, n, 0, count, binaries);
-
-  return binaries;
-}
-
-void freeBinaries(int **binaries, int n)
-{
-  // Libera a memória alocada para os binários
-  int totalBinaries = 1 << n;
-  for (int i = 0; i < totalBinaries; i++)
-  {
-    free(binaries[i]);
-  }
-  free(binaries);
-}
